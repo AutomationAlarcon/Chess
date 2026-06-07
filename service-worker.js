8787c8313da625273df9291733a25b21b69e26a3
alfonso <sitoalar@gmail.com>
2026-06-07 11:12:49 +0200

    Add installable chess PWA

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'chess-stockfish-pwa-v1';
+
+const APP_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './chess_site_v2_stockfish_test.html',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './stockfish/Copying.txt',
+  './stockfish/stockfish-10.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key === CACHE_NAME ? null : caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
